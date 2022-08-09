78067c5de052baabc56f94d96b9ab5ee73061780
SajjadSedaqat <Sajjad.Sedaqat@Gmail.com>
2022-08-09 17:48:42 +0430

    Add JDBC Repository for Ingredients.

---
+++ src/main/java/sajjad/tacos/data/IngredientRepository.java
@@ -0,0 +1,13 @@
+package sajjad.tacos.data;
+
+import sajjad.tacos.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
