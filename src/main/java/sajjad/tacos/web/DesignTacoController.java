5148b6539e1e387bf77344c5cb310fc85d83757d
SajjadSedaqat <Sajjad.Sedaqat@Gmail.com>
2022-08-09 17:53:51 +0430

    Change the code to use Ingredient Repository

--- src/main/java/sajjad/tacos/web/DesignTacoController.java
+++ src/main/java/sajjad/tacos/web/DesignTacoController.java
@@ -1,6 +1,7 @@
 package sajjad.tacos.web;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 import sajjad.tacos.Ingredient;
 import sajjad.tacos.Taco;
@@ -10,11 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import sajjad.tacos.Ingredient.Type;
+import sajjad.tacos.data.IngredientRepository;
 
 import javax.validation.Valid;
 
@@ -24,26 +26,22 @@ import javax.validation.Valid;
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+    private final IngredientRepository ingredientRepo;
+
+    @Autowired
+    public DesignTacoController(
+            IngredientRepository ingredientRepository) {
+        this.ingredientRepo = ingredientRepository;
+    }
+
 
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
-
+        Iterable<Ingredient> ingredients = ingredientRepo.findAll();
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
-
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
         }
 
     }
@@ -74,9 +72,12 @@ public class DesignTacoController {
         return "redirect:/orders/current";
     }
 
-    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+    private Iterable<Ingredient> filterByType(Iterable<Ingredient> ingredients, Type type) {
 
-        return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+        return StreamSupport.stream(ingredients.spliterator(), false)
+                .filter(i -> i.getType().equals(type))
+                .collect(Collectors.toList());
+
     }
 }
 
