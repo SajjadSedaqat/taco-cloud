8d8f9033b1c3b9c3c7fe36d5fd6419f20fe05922
SajjadSedaqat <Sajjad.Sedaqat@Gmail.com>
2022-08-25 14:11:57 +0430

    Update Interfaces to include Spring Data JDBC

--- src/main/java/sajjad/tacos/data/IngredientRepository.java
+++ src/main/java/sajjad/tacos/data/IngredientRepository.java
@@ -1,13 +1,10 @@
 package sajjad.tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
 import sajjad.tacos.Ingredient;
 
 import java.util.Optional;
 
-public interface IngredientRepository {
-    Iterable<Ingredient> findAll();
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
 }
--- src/main/java/sajjad/tacos/data/OrderRepository.java
+++ src/main/java/sajjad/tacos/data/OrderRepository.java
@@ -1,7 +1,8 @@
 package sajjad.tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
 import sajjad.tacos.TacoOrder;
 
-public interface OrderRepository {
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+
 }
